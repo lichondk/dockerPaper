62fc436756241a48c8ba31664d4b9a71d0a5a6bf
Anders Lichon <lichondk@gmail.com>
2018-11-05 21:43:50 +0100

    fix

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const fs = require('fs')
-var http = require('http');
+var express = require('express')
+var app = express()
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+app.get('/', function(req,res) {
     fs.readFile('test1.json', 'utf8', function(err, contents){
-        data = JSON.parse(contents)
-        var i, len = data.results.length, el, j;
-  
-        for(i = 1; i<len; i++){
-        el = data.results[i].location.coordinates.latitude;
-        j = i;
-    
-        while(j>0 && data.results[j-1]> el){
-            data.results[j] = data.results[j-1];
-            j--;
-        }
-    
-        data.results[j] = el;
-        }
-        //console.log(data)
-        res.end(x)
-    })
+    data = JSON.parse(contents)
+    var i, len = data.results.length, el, j;
 
-    
-}).listen(process.env.PORT || 8080);
+    for(i = 1; i<len; i++){
+    el = data.results[i].location.coordinates.latitude;
+    j = i;
+
+    while(j>0 && data.results[j-1]> el){
+        data.results[j] = data.results[j-1];
+        j--;
+    }
+
+    data.results[j] = el;
+    }
+    //console.log(data)
+    res.send(data)
+})
+}) 
+//insertionSort()
+var port = process.env.PORT || 1337;
+app.listen(port, function(){
+console.log('port 1337')
+})
